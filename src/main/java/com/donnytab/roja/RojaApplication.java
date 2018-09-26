6e5ec5ac760cf0851249a9336b9210f1f2cd97b3
downson <downson.yuen@gmail.com>
2018-09-25 23:23:41 -0400

    Initial commit

---
+++ src/main/java/com/donnytab/roja/RojaApplication.java
@@ -0,0 +1,12 @@
+package com.donnytab.roja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RojaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/donnytab/roja/RojaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.donnytab.roja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RojaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
