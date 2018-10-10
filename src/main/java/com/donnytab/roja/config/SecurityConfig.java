feb1e1a1bfeb55a1221c5b3b9630a0784df535ab
downson <downson.yuen@gmail.com>
2018-10-09 23:31:25 -0400

    Add SecurityConfig

---
+++ src/main/java/com/donnytab/roja/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.donnytab.roja.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .failureForwardUrl("/error")
+                .permitAll()                                // @TODO
+                .and()
+                .logout().logoutSuccessUrl("/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+}
